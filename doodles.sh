a99354619b6b17190857e2701aa5712678043304
Nii Golightly <ch1ris23@ymail.com>
2022-08-30 21:41:23 -0500

    clocking out

--- doodles.sh
+++ doodles.sh
@@ -5,6 +5,10 @@ case $run in
   -c)
     run='gcc'
     ;;
+    -cc)
+    run='g++'
+    ;;
+    
 
 esac
 
@@ -14,25 +18,15 @@ function getfile() {
     do
     # echo $dir
     if [[ $dir =~ "$1/" ]]; then
-        # echo "Directory $dir exists"
         for file in `ls -d $dir*`
         do 
             if [[ $file =~ "$3.c" ]]; then
-                # echo "File $file exists"
-                # echo "Compiling $file"
-                # check if the file exists
                 if [ -f $file ]; then
-                    # check if the file is executable
                     if [ -x $file ]; then
-                        # echo "File is executable"
-                        # echo "Running $file"
                         ./$file
                     else
-                        # echo "File is not executable"
-                        # echo "Compiling $file"
                         $run $file -o $file.out
                         echo "Running $file"
-                        # ./$file.out
                     fi
                 else
                     echo "File does not exist"
