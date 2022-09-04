f3a46859cdb33d41264628cfb7119ebe53a6b5a7
Nii Golightly <ch1ris23@ymail.com>
2022-09-03 19:40:36 -0500

    cool fun

--- C_and_CC/worker.cc
+++ C_and_CC/worker.cc
@@ -2,12 +2,35 @@
 
 
 
+int multiple(int a, int b) {
+  return a * b;
+}
+
+
+
+
+
+
+
 void repeat_10(){
     // counting starts at 0
     for (int i = 0; i < 10; i++)
     {
     
-     std::cout << "if you keep pushing after this point, you will create great things!" << std::endl;
+     int result = multiple(i, 10);
+    //  std::cout << "the result is: " << result << std::endl;
+     // if the result is greater than 100, print it
+        if (result > 10)
+        {
+            std::cout << "the result is greater than 10 : " << result << std::endl;
+        }  
+        else
+        {
+            std::cout << "the result is less than : " << result << std::endl;
+        }
+
+    //  std::cout << result << std::endl;
+     // print the result to the stdout and text 
     
       }
     
